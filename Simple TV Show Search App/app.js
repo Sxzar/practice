c8675c307911706e46cd39635617230208ee2a95
Sxzar <contact.sxzar@gmail.com>
2022-07-02 18:15:27 +0300

    Add functionality - remove images on new query

--- Simple TV Show Search App/app.js
+++ Simple TV Show Search App/app.js
@@ -1,11 +1,15 @@
 const form = document.querySelector('#searchForm');
+
+
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const config = {params : { q: searchTerm}}
     const response = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    removeImages();
     getImages(response.data);
     form.elements.query.value = '';
+
 });
 
 const getImages = (shows) => {
@@ -14,6 +18,15 @@ const getImages = (shows) => {
             const img = document.createElement('img');
             img.src =  result.show.image.medium;
             document.body.append(img);
+        }
+    }
+}
+
+const removeImages = () => {
+    let findImages = document.querySelectorAll('img');
+    if (findImages.length > 0){
+        for (let img of findImages) {
+            img.parentNode.removeChild(img);
         }
     }
 }
